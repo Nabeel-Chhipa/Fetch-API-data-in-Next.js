be1fe8b348eaac91b553ee058d190414044a2b2b
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-01-14 23:23:23 +0500

    update Link compnent style

--- app/nav-bar.js
+++ app/nav-bar.js
@@ -1,10 +1,23 @@
+'use client'
 import Link from 'next/Link';
 
 const navBar = () => {
   return (
-    <>
-      <Link href="/">Home</Link>
-      <Link href="/posts">Posts</Link>
+      
+      <>
+      <style jsx>
+        {`
+            .navWrapper {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+        `}
+      </style>
+    <nav className='navWrapper'>
+      <Link href="/" style={{margin: '0px 10px', color: '#000', textDecoration: 'none'}}>Home</Link>
+      <Link href="/posts" style={{margin: '0px 10px', color: '#000', textDecoration: 'none'}}>Posts</Link>
+    </nav>
     </>
   );
 };
