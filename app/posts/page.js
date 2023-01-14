b58141bfa5077b3d088c866ec772674e416d63bc
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-01-14 23:59:23 +0500

    get post title id

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { use } from "react";
 
 async function getPosts() {
@@ -10,7 +11,7 @@ const page = () => {
   return (
     <div>
       <ul>
-        {posts?.map((post) => <li key={post.id}>{post.title}</li> )}
+        {posts?.map((post) => <li key={post.id}><Link href={`/posts/${post.id}`}>{post.title}</Link></li> )}
       </ul>
     </div>
   );
